8882e5f3a4fef51f35b57877d9618e6fc7296bd1
Rob Harries <rob@igecko.com>
2021-11-06 02:01:37 -0700

    Remove literal reference to img_dims and region_dims

--- models/model_registry.py
+++ models/model_registry.py
@@ -5,18 +5,18 @@ from .util import *
 def get_model(name):
     if name == "simple_conv_vae_1":
         import models.architectures.simple_conv_1 as m
-        model = VaeGanHandler(name, (64, 64), (20, 20), m.Encoder(), m.Decoder(), m.Discriminator())
+        model = VaeGanHandler(name, m.IMG_DIMS, m.REGION_DIMS, m.Encoder(), m.Decoder(), m.Discriminator())
         model.GAN_RELATIVE_LOSS_WEIGHT = 0
         return model
 
     if name == "simple_conv_vae_gan_1":
         import models.architectures.simple_conv_1 as m
-        model = VaeGanHandler(name, (64, 64), (20, 20), m.Encoder(), m.Decoder(), m.Discriminator())
+        model = VaeGanHandler(name, m.IMG_DIMS, m.REGION_DIMS, m.Encoder(), m.Decoder(), m.Discriminator())
         return model
 
     if name == "simple_conv_gan_1":
         import models.architectures.simple_conv_1 as m
-        model = VaeGanHandler(name, (64, 64), (20, 20), m.Encoder(), m.Decoder(), m.Discriminator())
+        model = VaeGanHandler(name, m.IMG_DIMS, m.REGION_DIMS, m.Encoder(), m.Decoder(), m.Discriminator())
         model.GAN_RELATIVE_LOSS_WEIGHT = 1
         model.RECON_RELATIVE_LOSS_WEIGHT = 0
         model.KL_RELATIVE_LOSS_WEIGHT = 0
@@ -24,12 +24,12 @@ def get_model(name):
 
     if name == "resnet_conv_vae_gan_1":
         import models.architectures.resnet_conv_1 as m
-        model = VaeGanHandler(name, (64, 64), (20, 20), m.Encoder(), m.Decoder(), m.Discriminator())
+        model = VaeGanHandler(name, m.IMG_DIMS, m.REGION_DIMS, m.Encoder(), m.Decoder(), m.Discriminator())
         return model
 
     if name == "resnet_conv_vae_1":
         import models.architectures.resnet_conv_1 as m
-        model = VaeGanHandler(name, (64, 64), (20, 20), m.Encoder(), m.Decoder(), m.Discriminator())
+        model = VaeGanHandler(name, m.IMG_DIMS, m.REGION_DIMS, m.Encoder(), m.Decoder(), m.Discriminator())
         model.GAN_RELATIVE_LOSS_WEIGHT = 0
         return model
 
